1bddbbe2de5707beaaed7ac6afb48136dfeaff3a
Sam Deakin <sam_d1995@hotmail.com>
2017-02-19 20:18:39 -0800

    Updates some window creation code

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <string>
 
 #include <GL/glew.h>
 
@@ -7,59 +7,93 @@
 #include <glm/glm.hpp>
 
 using namespace glm;
+using namespace std;
 
 #define GLSL(src) #src
 
 static void error_callback(int error, const char *description) {
-  fputs(description, stderr);
+    fputs(description, stderr);
 }
 
 static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
-  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-    glfwSetWindowShouldClose(window, GL_TRUE);
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
 }
 
 static void render(GLFWwindow *window) {
-  glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_ACCUM_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 }
 
 int main(void) {
-  GLFWwindow *window;
+    GLFWwindow *window;
+    GLFWmonitor *monitor;
+    int window_width = 1280;
+    int window_height = 720;
+    int internal_width = 0;
+    int internal_height = 0;
+    string title = "Particles";
 
-  glfwSetErrorCallback(error_callback);
+    glfwSetErrorCallback(error_callback);
 
-  if (!glfwInit())
-    exit(EXIT_FAILURE);
+    if (glfwInit() == GL_FALSE) {
+        fprintf(stderr, "Error calling glfwInit.\n");
+        std::abort();
+    }
 
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_VISIBLE, GL_TRUE);
+    glfwWindowHint(GLFW_SAMPLES, 0);
+    glfwWindowHint(GLFW_RED_BITS, 8);
+    glfwWindowHint(GLFW_GREEN_BITS, 8);
+    glfwWindowHint(GLFW_BLUE_BITS, 8);
+    glfwWindowHint(GLFW_ALPHA_BITS, 8);
 
-  window = glfwCreateWindow(640, 480, "OpenGL Boilerplate", NULL, NULL);
-  if (!window) {
+    monitor = glfwGetPrimaryMonitor();
+    if (monitor == NULL) {
+        glfwTerminate();
+        fprintf(stderr, "Error calling glfwGetPrimaryMonitor.\n");
+        abort();
+    }
+
+    window = glfwCreateWindow(window_width, window_height, title.c_str(), NULL, NULL);
+    if (window == NULL) {
+        glfwTerminate();
+        fprintf(stderr, "Error calling glfwCreateWindow.\n");
+        abort();
+    }
+
+    // Update internal size
+    glfwGetFramebufferSize(window, &internal_width, &internal_height);
+    glfwGetWindowSize(window, &window_width, &window_height);
+    cout << internal_width << " x " << internal_height << " window: " << window_width << " x " << window_height << endl;
+
+    glewExperimental = GL_TRUE;
+    glfwMakeContextCurrent(window);
+    glewInit();
+
+    glfwSetKeyCallback(window, key_callback);
+    // glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    glfwSwapInterval(1);
+
+    while (!glfwWindowShouldClose(window)) {
+        render(window);
+
+        // Update internal size
+        glfwGetFramebufferSize(window, &internal_width, &internal_height);
+        glfwGetWindowSize(window, &window_width, &window_height);
+        cout << internal_width << " x " << internal_height << " window: " << window_width << " x " << window_height << endl;
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
     glfwTerminate();
-    exit(EXIT_FAILURE);
-  }
-
-  glewExperimental = GL_TRUE;
-  glewInit();
-
-  glfwMakeContextCurrent(window);
-
-  glfwSetKeyCallback(window, key_callback);
-
-  while (!glfwWindowShouldClose(window)) {
-    render(window);
-
-    glfwSwapBuffers(window);
-    glfwPollEvents();
-    //glfwWaitEvents();
-  }
-
-  glfwDestroyWindow(window);
-
-  glfwTerminate();
-  exit(EXIT_SUCCESS);
+    exit(EXIT_SUCCESS);
 }
